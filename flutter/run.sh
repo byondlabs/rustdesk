3f8eef8b98edbc263b58cb8817e3560fe95fa9db
r-byondlabs <byondlabs.tech>
2024-07-04 21:26:22 +0530

    Changes for local dev

--- flutter/run.sh
+++ flutter/run.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
+set -ex
 
 cargo install flutter_rust_bridge_codegen --version 1.80.1 --features uuid
 flutter pub get
+
 ~/.cargo/bin/flutter_rust_bridge_codegen --rust-input ../src/flutter_ffi.rs --dart-output ./lib/generated_bridge.dart --c-output ./macos/Runner/bridge_generated.h
+
 # call `flutter clean` if cargo build fails
 # export LLVM_HOME=/Library/Developer/CommandLineTools/usr/
-cargo build --features flutter
+# cargo build --features flutter
+#
+# r-byondlabs: taken from github-build
+# MACOSX_DEPLOYMENT_TARGET=10.14 cargo build --features flutter --lib --release
+MACOSX_DEPLOYMENT_TARGET=10.14 cargo build --features flutter --lib
+# copy dylib
+cp ../target/debug/liblibrustdesk.dylib ../target/release/liblibrustdesk.dylib
+
 flutter run $@
